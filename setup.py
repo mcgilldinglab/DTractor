34e2032108b904aeacc96f33c976976a21d8da33
Chenyu Liu <44542822+theguardsgod@users.noreply.github.com>
2025-03-08 00:23:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,14 @@ setup(
         "numpy==1.23.5",
         "pandas==1.4.2",
         "plotly==5.6.0",
-        "scanpy==1.9.5",
+        "scanpy==1.11.0",
         "scikit_learn==1.0.2",
         "scipy==1.9.0",
-        "scvi-tools == 0.20.2",
+        "scvi-tools == 0.6.8",
         "seaborn==0.11.2",
-        "statsmodels==0.13.2"
+        "statsmodels==0.13.2",
+        "torch==1.13.0+cu117",
+        
     ],
 )
 
