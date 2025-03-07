3b41fce80d3ba3914b72ab47b847ad8cb0df6d6c
Jin <yjkweon24@berkeley.edu>
2025-03-07 02:22:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="DTractor",  # The name of your package
-    version="0.1",
+    version="0.1.0",
+    description='DTractor package 0.1.0',
+    author="Jin Kweon",
+    author_email="yong.kweon@mail.mcgill.ca", 
+    url="https://github.com/mcgilldinglab/DTractor",
     packages=find_packages(),  # Automatically finds the package (DTractor)
+    classifiers=['Programming Language :: Python :: 3.9'],
     install_requires=[         # List any dependencies your package has
         "matplotlib==3.7.2",
         "numpy==1.23.5",
@@ -18,3 +23,5 @@ setup(
         "torch==1.13.0+cu117",
     ],
 )
+
+
